3637b237cd0a7285710378320fffdecb427b5c2a
Pedro Amui <curiamuiph@gmail.com>
2017-11-03 17:37:58 -0700

    oof

--- 1010N Fall Qualifier/include/ports.h
+++ 1010N Fall Qualifier/include/ports.h
@@ -2,25 +2,25 @@
 #define PORTS_H_
 
 //Drive Right
-#define DRIVE_RF 3
-#define DRIVE_RB 6 //Power Expander D
+#define DRIVE_RF 2
+#define DRIVE_RB 3 //Power Expander D
 
 //Drive Left
-#define DRIVE_LF 4 //Power Expander C
-#define DRIVE_LB 7
+#define DRIVE_LF 9 //Power Expander C
+#define DRIVE_LB 8
 
 //Arm
-#define ARM_R 2
-#define ARM_L1 8 //Power Expander A
-#define ARM_L2 9
+#define ARM_R 5
+#define ARM_L1 6 //Power Expander A
+#define ARM_L2 7
 
 //Mobile Goal Lift
-#define LIFT 10
+#define LIFT 1
 
 //Claw
-#define INTAKE 1
+#define INTAKE 10
 
 //Four Bar
-#define FOURBAR 5 //Power Expander B
+#define FOURBAR 4 //Power Expander B
 
 #endif
